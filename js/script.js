7d92aaa690ec505141c822bbc7b737aef055989d
Gavin Pereira <gavineditz@gmail.com>
2021-04-10 11:01:21 +0530

    hotjar code added to script.js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ $(window).on("scroll", function() {
        $(".header").removeClass("active");
     }
 });
+
+
+
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2323400,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
